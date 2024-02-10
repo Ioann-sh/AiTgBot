aa95a35f1998229a78e9e12189a747e057663dcd
Ioann-sh <80589546+Ioann-sh@users.noreply.github.com>
2024-02-10 20:04:03 +0400

    feat: add method deleteContext

--- manager/DB/DB.py
+++ manager/DB/DB.py
@@ -13,7 +13,8 @@ class DB:
         print("database connection closed")
 
     def getUserByUserId(self, user_id):
-        self.cursor.execute("SELECT * FROM users WHERE user_id=:user_id", {"user_id": user_id})
+        self.cursor.execute("SELECT * FROM users WHERE user_id=:user_id",
+                            {"user_id": user_id})
         return self.cursor.fetchone()
 
     def registration(self, name, user_id):
@@ -23,14 +24,20 @@ class DB:
         return self.conn.commit()
 
     def getContext(self, user_id):
-        self.cursor.execute("SELECT message FROM context WHERE user_id=:user_id  ORDER BY timestamp DESC LIMIT 1",
+        self.cursor.execute("SELECT message FROM context WHERE user_id=:user_id ORDER BY timestamp DESC LIMIT 1",
                             {"user_id": user_id})
         row = self.cursor.fetchone()
         context = row[0] if row else ""
+        # context = row[0].split()[-1] if row else ""
         return context
 
     def addContext(self, user_id, message, timestamp):
         query = "INSERT INTO context (user_id, message, timestamp) VALUES (:user_id, :message, :timestamp)"
         val = (user_id, message, timestamp)
         self.cursor.execute(query, val)
+        return self.conn.commit()
+
+    def deleteContext(self, user_id):
+        self.cursor.execute("DELETE FROM context WHERE user_id=:user_id",
+                            {"user_id": user_id})
         return self.conn.commit()
