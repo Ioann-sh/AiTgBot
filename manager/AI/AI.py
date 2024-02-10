fe04a4098467032e6db3ea227b22e33dc675da5d
Ioann-sh <80589546+Ioann-sh@users.noreply.github.com>
2024-02-10 11:48:13 +0400

    update: context added to request

--- manager/AI/AI.py
+++ manager/AI/AI.py
@@ -1,30 +1,19 @@
 import openai
 
 
+
 class AI:
 
     def __init__(self, settings):
-        openai.api_key = settings['API_KEY']  # APIkey
-        openai.Model.list()
+        openai.api_key = settings['API_KEY']
 
     @staticmethod
-    def chat(message):
+    def chat(message, context):
         completion = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=[
-                {"role": "user", "content": f"{message}"}
+                {"role": "user", "content": message + context}
             ]
         )
-        chat_response = completion.choices[0].message.content
-        return chat_response
-
-    @staticmethod
-    def chatSys(message):
-        completion = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": "system", "content": f"{message}"}
-            ]
-        )
-        chat_response = completion.choices[0].message.content
+        chat_response = completion.choices[0].text
         return chat_response
