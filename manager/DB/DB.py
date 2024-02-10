40cfcad56ed6dda0f398175c31bb6d724f7ac52c
Ioann-sh <80589546+Ioann-sh@users.noreply.github.com>
2024-02-10 11:46:17 +0400

    add: getContext, addContext

--- manager/DB/DB.py
+++ manager/DB/DB.py
@@ -21,3 +21,16 @@ class DB:
         val = (name, user_id)
         self.cursor.execute(query, val)
         return self.conn.commit()
+
+    def getContext(self, user_id):
+        self.cursor.execute("SELECT message FROM context WHERE user_id=:user_id  ORDER BY timestamp DESC LIMIT 1",
+                            {"user_id": user_id})
+        row = self.cursor.fetchone()
+        context = row[0] if row else ""
+        return context
+
+    def addContext(self, user_id, message, timestamp):
+        query = "INSERT INTO context (user_id, message, timestamp) VALUES (:user_id, :message, :timestamp)"
+        val = (user_id, message, timestamp)
+        self.cursor.execute(query, val)
+        return self.conn.commit()
